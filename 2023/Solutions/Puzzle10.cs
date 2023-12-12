a35b50680be805934d8333ba9ecdf8cb14988b7b
Filip Najdovski <filip.najdovski@outlook.com>
2023-12-12 08:14:40 +0800

    2023 Puzzle 10 Part 2

--- 2023/Solutions/Puzzle10.cs
+++ 2023/Solutions/Puzzle10.cs
@@ -53,7 +53,7 @@ internal class Puzzle10 : Puzzle2023<Puzzle10>
         Pipe[startRow, startCol] = true;
 
         // Kinda ugly but eh
-        if (File[startRow - 1][startCol] is '|' or '7' or 'F') Enqueue(startRow - 1, startCol); // Up
+        // if (File[startRow - 1][startCol] is '|' or '7' or 'F') Enqueue(startRow - 1, startCol); // Up
         if (File[startRow + 1][startCol] is '|' or 'L' or 'J') Enqueue(startRow + 1, startCol); // Down
         if (File[startRow][startCol - 1] is '-' or 'L' or 'F') Enqueue(startRow, startCol - 1); // Left
         if (File[startRow][startCol + 1] is '-' or '7' or 'J') Enqueue(startRow, startCol + 1); // Right
@@ -86,8 +86,117 @@ internal class Puzzle10 : Puzzle2023<Puzzle10>
         Pipe![row, col] = true;
     }
 
+    /*
+     * I hate it but essentially we search for gaps between pipes
+     * For example, pipe 'L' creates a gap to its left and down and under
+     * E.g., JL creates [space · pipe · space · pipe · space] meaning there is a gap between them
+     * Iterate over all points in this new gap space. If gaps lead to the edge of the map, then not enclosed
+     * Couldn't figure out best way to memoise and save computation, so we do complete BFS for every non-pipe
+    */
     internal override void Part2()
     {
-        // TODO
+        Part1();
+        bool[,] pipes = new bool[2 * N + 1, 2 * M + 1];
+        
+        // Map pipes
+        for (var row = 0; row < N; row++)
+        {
+            for (var col = 0; col < M; col++)
+            {
+                if (!Pipe![row, col]) continue;
+                var pipeType = File[row][col];
+
+                var r = 2 * row + 1;
+                var c = 2 * col + 1;
+                pipes[r, c] = true;
+
+                if (pipeType is '|')
+                {
+                    pipes[r - 1, c] = true;
+                    pipes[r + 1, c] = true;
+                }
+                else if (pipeType is '-')
+                {
+                    pipes[r, c - 1] = true;
+                    pipes[r, c + 1] = true;
+                }
+                else if (pipeType is 'L')
+                {
+                    pipes[r - 1, c] = true;
+                    pipes[r, c + 1] = true;
+                }
+                else if (pipeType is 'J')
+                {
+                    pipes[r - 1, c] = true;
+                    pipes[r, c - 1] = true;
+                }
+                else if (pipeType is '7')
+                {
+                    pipes[r + 1, c] = true;
+                    pipes[r, c - 1] = true;
+                }
+                else if (pipeType is 'F')
+                {
+                    pipes[r + 1, c] = true;
+                    pipes[r, c + 1] = true;
+                }
+            }
+        }
+
+        var area = 0L;
+        for (var row = 0; row < N; row++)
+        {
+            for (var col = 0; col < M; col++)
+            {
+                if (row == 3 && col == 14)
+                {
+
+                }
+                if (Pipe![row, col]) continue;
+
+                var r = 2 * row + 1;
+                var c = 2 * col + 1;
+
+                Visited = new bool[2 * N + 1, 2 * M + 1];
+                var n = 2 * N + 1;
+                var m = 2 * M + 1;
+                Queue<(int r, int c)> queue = new();
+                queue.Enqueue((r, c));
+
+                bool TryEnqueue(int r, int c, out bool isEnclosed)
+                {
+                    isEnclosed = true;
+                    if (r < 0 || r >= n || c < 0 || c >= m)
+                    {
+                        isEnclosed = false;
+                        return false;
+                    }
+
+                    if (Visited![r, c] || pipes[r, c]) return false;
+
+                    Visited[r, c] = true;
+                    queue.Enqueue((r, c));
+                    return true;
+                }
+
+                var isAreaEnclosed = true;
+                while (queue.Count > 0)
+                {
+                    var node = queue.Dequeue();
+
+                    TryEnqueue(node.r - 1, node.c, out var isUpEnclosed);
+                    TryEnqueue(node.r + 1, node.c, out var isDownEnclosed);
+                    TryEnqueue(node.r, node.c - 1, out var isRightEnclosed);
+                    TryEnqueue(node.r, node.c + 1, out var isLeftEnclosed);
+
+                    isAreaEnclosed = isAreaEnclosed && isUpEnclosed && isDownEnclosed && isRightEnclosed && isLeftEnclosed;
+                }
+
+                if (isAreaEnclosed)
+                    area++;
+            }
+        }
+
+        area.Dump("Part 2 Answer");
     }
 }
