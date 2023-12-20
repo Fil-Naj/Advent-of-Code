a6f02351c11e464e6300d90254b8c0ff020cc150
Filip Najdovski <filip.najdovski@outlook.com>
2023-12-20 15:05:39 +0800

    2023 Puzzle 18

--- 2023/Solutions/Puzzle18.cs
+++ 2023/Solutions/Puzzle18.cs
@@ -4,92 +4,69 @@ namespace AdventOfCode._2023.Solutions;
 
 internal class Puzzle18 : Puzzle2023<Puzzle18>
 {
-    private static readonly Dictionary<string, (int x, int y)> _directions = new()
-    {
-        { "U", (0, -1) },
-        { "D", (0, 1) },
-        { "L", (-1, 0) },
-        { "R", (1, 0) },
-    };
+    // Shoutout Green's theorem
+    // Much better than Blue's theorem
+    // Essententially, the area of any flat (i.e., only has straight lines), 2D shape can be calculated using Green's Theorem
+    // Don't even worry about curves, too much math
+    // Green's Theorem states that an on object's area is equal to the sum of one of its coordinates multiplied by the delta of the opposing coordinate at each vertex
+    // In simple terms, A = sum(y * dx) = sum(x * dy)
+    // Pick whether we will track y or x. For this explanation, we will choose to track y as we have done so in the code
+    // Then, for each vertex, if the instruction is a change in y (i.e. up or down move), keep track of y so we know the y of each vertex
+    // When we encounter a change in x (i.e., left or right), we multiply the y coordinate by the change in x (in this case dx is numSteps)
+    // In our axis, left is a negative change in x, while right is positive dx
+    // Sum up these y * dx values for each vertex
+    // And BOOM! You have the area of some random straight lined shape
     internal override void Part1()
     {
-        HashSet<(int x, int y)> points = [(0,0)];
-        var x = 0;
-        var y = 0;
-        foreach (var line in File)
-        {
-            var instructions = line.Split(' ');
-            var delta = _directions[instructions[0]];
-            var numSteps = int.Parse(instructions[1]);
-
-            for (int i = 0; i < numSteps; i++)
-            {
-                x += delta.x;
-                y += delta.y;
-                points.Add((x,y));
-            }
-        }
-        var minX = points.Min(x => x.x);
-        var minY = points.Min(x => x.y);
-        var maxX = points.Max(x => x.x);
-        var maxY = points.Max(x => x.y);
-
-        var n = maxY - minY + 1;
-        var m = maxX - minX + 1;
-        int[,] steps = new int[n, m];
-        foreach (var point in points)
-        {
-            steps[point.y - minY, point.x - minX] = 1;
-        }
-        
-        var visited = new bool[n, m];
-        var area = 0L;
-        // BFS each point in step
-        for (int row = 0; row < maxY; row++)
-        {
-            for (int col = 0; col < maxX; col++)
-            {
-                // If visited or is the outline, next spot
-                if (visited[row, col] || steps[row, col] == 1) continue;
-                Queue<(int r, int c)> queue = new();
-                queue.Enqueue((row, col));
-
-                var isTouchingEdge = false;
-                var count = 1;
-                void TryEnqueue(int r, int c)
-                {
-                    if (r < 0 || r >= n || c < 0 || c >= m)
-                    {
-                        isTouchingEdge = true;
-                        return;
-                    }
-
-                    if (visited[r, c] || steps[r, c] == 1) return;
-
-                    count++;
-                    visited[r, c] = true;
-                    queue.Enqueue((r, c));
-                }
-
-                while (queue.Count > 0)
-                {
-                    var (r, c) = queue.Dequeue();
-
-                    TryEnqueue(r + 1, c);
-                    TryEnqueue(r - 1, c);
-                    TryEnqueue(r, c + 1);
-                    TryEnqueue(r, c - 1);
-                }
-
-                area += isTouchingEdge ? 0 : count;
-            }
-        }
-
-        (area + points.Count - 1).Dump("Part 1 Answer");
+        GetAreaGreensTheoremStyle(1).Dump("Part 1 Answer");
     }
 
     internal override void Part2()
     {
-        
+        GetAreaGreensTheoremStyle(2).Dump("Part 1 Answer");
+    }
+
+    private long GetAreaGreensTheoremStyle(int partNumber)
+    {
+        var area = 0L;
+        var y = 0L;
+        var perimeter = 0L;
+        foreach (var line in File)
+        {
+            var instructions = line.Split(' ');
+            string direction;
+            long numSteps;
+            if (partNumber == 1)
+            {
+                direction = instructions[0];
+                numSteps = int.Parse(instructions[1]);
+            }
+            else
+            {
+                var hexNum = instructions[2][2..7];
+                numSteps = Convert.ToInt64(instructions[2][2..7], 16);
+                direction = instructions[2][7] switch
+                {
+                    '0' => "R",
+                    '1' => "D",
+                    '2' => "L",
+                    '3' => "U",
+                    _ => string.Empty
+                };
+            }
+
+            perimeter += numSteps;
+
+            if (direction is "L")
+                area -= y * numSteps;
+            else if (direction is "R")
+                area += y * numSteps;
+            else if (direction == "U")
+                y += numSteps;
+            else
+                y -= numSteps;
+        }
+
+        return area + (perimeter / 2) + 1;
     }
 }
